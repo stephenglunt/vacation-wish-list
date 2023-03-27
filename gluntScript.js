f3765fbdc5f9aa16aee4b8a7962c74c11eb81937
Stephen Glunt <stephen.glunt@tlgcohort.com>
2023-03-27 16:55:09 -0700

    have add cards working. Still need validation

--- gluntScript.js
+++ gluntScript.js
@@ -1,54 +1,98 @@
 document.addEventListener("DOMContentLoaded", function () {
   const destinations = document.querySelector('#destinations_container');
+  const inputButton = document.querySelector('#inputButton');
+
+
+  inputButton.addEventListener('click', () => {
+    const newCard = createNewCard();
+
+    let valid = (validateName && validateLocation && validatePhoto);
+    if(!valid) return;
+
+
+    destinations.appendChild(newCard.card);
+
+
+    newCard.editButton.addEventListener('click', () => {
+      //make edit function here
+    });
+
+    newCard.removeButton.addEventListener('click', () => {
+      newCard.card.remove();
+    });
+  });
+
+});
+
+function createNewCard() {
   const nameInput = document.querySelector('#name');
   const locationInput = document.querySelector('#location');
   const photoURLInput = document.querySelector('#photo');
   const descriptionInput = document.querySelector('#description');
-  const inputButton = document.querySelector('#inputButton');
 
-  inputButton.addEventListener('click', () => {
-    //create new card
-    const newCard = document.createElement('div');
-    const image = document.createElement('img');
-    image.src = photoURLInput.value;
-    const name = document.createElement('h2');
-    name.textContent = nameInput.value;
-    const location = document.createElement('p');
-    location.className = 'location';
-    location.textContent = locationInput.value;
-    const description = document.createElement('p');
-    description.className = 'description';
-    description.textContent = descriptionInput.value;
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Edit';
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
+  const newCardObj = {
+    card: document.createElement('div'),
+    image: document.createElement('img'),
+    name: document.createElement('h2'),
+    location: document.createElement('p'),
+    description: document.createElement('p'),
+    editButton: document.createElement('button'),
+    removeButton: document.createElement('button')
+  };
 
-    newCard.className = 'card';
+  //set classes
+  let classNames = ['card', 'image', 'destination', 'location', 'description', 'button', 'button'];
+  let i = 0;
+  for (let element in newCardObj) {
+    newCardObj[element].className = classNames[i];
+    i++;
+  }
 
-    newCard.appendChild(image);
-    newCard.appendChild(name);
-    newCard.appendChild(location);
-    newCard.appendChild(description);
-    newCard.appendChild(editButton);
+  //set content
+  newCardObj.image.src = photoURLInput.value;
+  newCardObj.name.textContent = nameInput.value;
+  newCardObj.location.textContent = locationInput.value;
+  newCardObj.description.textContent = descriptionInput.value;
+  newCardObj.editButton.textContent = 'Edit';
+  newCardObj.removeButton.textContent = 'Remove';
 
-    newCard.appendChild(removeButton);
-    destinations.appendChild(newCard);
+  //append children
+  newCardObj.card.appendChild(newCardObj.image);
+  newCardObj.card.appendChild(newCardObj.name);
+  newCardObj.card.appendChild(newCardObj.location);
+  newCardObj.card.appendChild(newCardObj.description);
+  newCardObj.card.appendChild(newCardObj.editButton);
+  newCardObj.card.appendChild(newCardObj.removeButton);
 
+  return newCardObj;
 
-    editButton.addEventListener('click', () => {
-      //make edit function here
-    });
-
-    removeButton.addEventListener('click', () => {
-      newCard.remove();
-    })
-  });
+}
 
 
 
-});
+function validateName() {
+  const name = document.getElementById("name").value;
+  if (name.length < 2) {
 
-let createNewCard = () => {
+    alert("Name must be at least 2 characters.");
+    return false;
+  }
+}
 
+function validateLocation() {
+  const location = document.getElementById("location").value;
+  if (location.length < 2) {
+    alert("Location must be at least 2 characters.");
+    return false;
+  }
+}
+
+function validatePhoto() {
+  const photo = document.getElementById("photo").value;
+  const imageExtensions = ["jpg", "jpeg", "png", "gif"];
+  const extension = photo.split(".").pop().toLowerCase();
+  if (!imageExtensions.includes(extension)) {
+    alert("Photo must be a valid image URL.");
+    return false
+  }
 }
