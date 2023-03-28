d10910b2fdeb2e32813374ad873c64ac5ed01dc7
Stephen Glunt <stephen.glunt@tlgcohort.com>
2023-03-27 20:04:11 -0700

    Got validation working with Edit and Cancel. Working on enabling editing of the img src.

--- gluntScript.js
+++ gluntScript.js
@@ -1,28 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
   const destinations = document.querySelector('#destinations_container');
   const inputButton = document.querySelector('#inputButton');
+  const cardData = { name: "", image_src: "", location: "", description: "" };
+
 
 
   inputButton.addEventListener('click', () => {
     const newCard = createNewCard();
 
-    let valid = (validateName && validateLocation && validatePhoto);
-    if(!valid) return;
-
+    if (!validateName() || !validateLocation()) {
+      // alert("Name must be at least 2 characters.");
+      return;
+    }
 
     destinations.appendChild(newCard.card);
 
-
     newCard.editButton.addEventListener('click', () => {
-      //make edit function here
+
+      editCard(newCard, cardData);
     });
 
     newCard.removeButton.addEventListener('click', () => {
-      newCard.card.remove();
+      if (newCard.removeButton.textContent === "Remove") {
+        newCard.card.remove();
+      } else {
+        cancelEdit(newCard, cardData);
+      }
     });
   });
 
+
 });
+
 
 function createNewCard() {
   const nameInput = document.querySelector('#name');
@@ -44,12 +53,20 @@ function createNewCard() {
   let classNames = ['card', 'image', 'destination', 'location', 'description', 'button', 'button'];
   let i = 0;
   for (let element in newCardObj) {
-    newCardObj[element].className = classNames[i];
+    newCardObj[element].className = classNames[i] + " input";
     i++;
   }
 
   //set content
-  newCardObj.image.src = photoURLInput.value;
+  let img = new Image();
+  img.src = photoURLInput.value;
+  img.onload = () => {
+    newCardObj.image.src = photoURLInput.value;
+  }
+  img.onerror = () => {
+    newCardObj.image.src = "https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg";
+  }
+
   newCardObj.name.textContent = nameInput.value;
   newCardObj.location.textContent = locationInput.value;
   newCardObj.description.textContent = descriptionInput.value;
@@ -72,27 +89,89 @@ function createNewCard() {
 
 function validateName() {
   const name = document.getElementById("name").value;
+  const errorSpan = document.getElementById("name-error");
   if (name.length < 2) {
-
-    alert("Name must be at least 2 characters.");
+    errorSpan.textContent = "Name must be at least 2 characters.";
     return false;
+  } else {
+    errorSpan.textContent = "";
   }
+  return true;
 }
 
 function validateLocation() {
   const location = document.getElementById("location").value;
+  const errorSpan = document.getElementById("location-error");
   if (location.length < 2) {
-    alert("Location must be at least 2 characters.");
+    errorSpan.textContent = "Location must be at least 2 characters.";
     return false;
+  } else {
+    errorSpan.textContent = "";
+  }
+  return true;
+}
+
+function editCard(card, cardData) {
+
+  if (card.editButton.textContent === "Edit") {
+    //Save original data
+    cardData.name = card.name.textContent;
+    cardData.image_src = card.image.src;
+    cardData.location = card.location.textContent;
+    cardData.description = card.description.textContent;
+
+    //Change button labels
+    card.editButton.textContent = "Save";
+    card.removeButton.textContent = "Cancel";
+
+    //Make Content Editable
+    card.name.contentEditable = true;
+    card.location.contentEditable = true;
+    card.description.contentEditable = true;
+
+    //Change img object to input object with same dimensions.
+    card.image.contentEditable = true;
+    card.image.setAttribute("draggable", false);
+    card.name.focus();
+  } else {
+
+    stopEditingCard(card, cardData);
   }
 }
 
-function validatePhoto() {
-  const photo = document.getElementById("photo").value;
-  const imageExtensions = ["jpg", "jpeg", "png", "gif"];
-  const extension = photo.split(".").pop().toLowerCase();
-  if (!imageExtensions.includes(extension)) {
-    alert("Photo must be a valid image URL.");
-    return false
+function cancelEdit(card, cardData) {
+  let img = new Image();
+  img.src = cardData.image_src;
+  img.onload = () => {
+    card.image.src = cardData.image_src;
+  }
+  img.onerror = () => {
+    card.image.src = "https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg";
+  }
+
+  card.name.textContent = cardData.name;
+  card.location.textContent = cardData.location;
+  card.description.textContent = cardData.description;
+
+  stopEditingCard(card, cardData);
+}
+
+function stopEditingCard(card, cardData) {
+  card.editButton.textContent = "Edit";
+  card.removeButton.textContent = "Remove";
+  card.name.contentEditable = false;
+  card.location.contentEditable = false;
+  card.description.contentEditable = false;
+  card.image.removeAttribute("draggable");
+  card.image.contentEditable = false;
+
+  console.log(card.location.textContent);
+  console.log(cardData.location);
+
+  if (card.name.textContent.length < 2) {
+    card.name.textContent = cardData.name;
+  }
+  if (card.location.textContent.length < 2) {
+    card.location.textContent = cardData.location;
   }
 }
